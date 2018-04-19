9e72ae420994044971a0b739f1091ca48f28580f
Christian Marosz <s8chmaro@stud.uni-saarland.de>
2018-04-20 00:08:16 +0200

    Alles Erledigt

--- Prog2/Test2.asm
+++ Prog2/Test2.asm
@@ -1,50 +1,158 @@
-.data
-a0text:
-	.asciiz "123456"
-buffer:
-	.space 8
-.text
+	.data
+testblz:
+	.asciiz "12345678"
+testknr:
+	.asciiz "1234567890"
+ibanbuf:
+	.space 23
+Iban22:
+	.space 23
+DE00:
+	.asciiz "DE00"
+	.globl knr2iban
+	.text
+# -- knr2iban
+# Argumente:
+# a0: IBAN buffer (22 Zeichen)
+# a1: BLZ buffer (8 Zeichen)
+# a2: KNR buffer (10 Zeichen)
 
-li $a0 15678
-la $a1 buffer
-li $a2 2
-
-
-# -- int_to_buf --
-# Eingaberegister:
-# a0: eine positive Zahl
-# a1: Startadresse des Buffers
-# a2: Anzahl der Stellen (von rechts)
-# Ausgaberegister: keine
-# Funktion:
-# Schreibt die ersten $a2 Ziffern der positiven Zahl in $a0 als dezimale Zeichenkette in den Speicherbereich [$a1 bis $a1 + $a2 - 1]
-int_to_buf:
-	move	$t2 $a1       # $t2 Startadresse des Buffers
-	add	$t3 $a1 $a2
-	subu	$t1 $t3 1   # $t1 Adresse des hoechsten Zeichens
-	move	$t0 $a0       # $t0 restliche Zahl
-
-ib_loop:
-# $t0 enthaelt die verbleibende Zahl
-# entferne die erste Dezimalstelle der Zahl in $t0
-# $t3 enthaelt die extrahierte Ziffer als Zahl
-	move	$t3 $t0
-	divu	$t0 $t0 10
-	mulu	$t4 $t0 10
-	subu	$t3 $t3 $t4 # $t3 = $t3 - (($t3 / 10) * 10)
-
-# Kodiere die Ziffer in $t3 als ein ASCII Zeichen und schreibe es nach $t1
-	addu	$t3 $t3 48
-	sb	$t3 ($t1)
-
-# Gehe zum naechsten Zeichen
-	subu	$t1 $t1 1
-	bgeu	$t1 $t2 ib_loop
-	
-	
-	move $a0 $a1
-	
-	print:
-	li	$v0 4
+	la	$a0 ibanbuf
+	la	$a1 testblz
+	addu	$sp $sp -4
+	sw 	$ra 0($sp)
+	la	$a2 testknr
+	jal	knr2iban
+	lw	$ra 0($sp)
+	addu	$sp $sp 4
+	li	$v0 10
+	la	$a0 ibanbuf
+	li	$a1 22
 	syscall
+	li	$v0 10
+	syscall
+
+knr2iban:
+	# TODO
+	
+	move 	$t6 $a0
+	move 	$t1 $a1
+	move 	$t2 $a2
+	
+	la 	$a1 Iban22
+	la	$a2 DE00
+	move	$t7 $ra
+	li	$t4 4
+	jal	DE002Iban
+	li	$t4 8
+	jal	Blz2Iban
+	li	$t4 10
+	jal	Knr2Iban
+	move	$ra $t7
+	
+	addu	$sp $sp -4
+	sw 	$ra 0($sp)
+	
+	jal 	validate_checksum
+	
+	lw	$ra 0($sp)
+	addu	$sp $sp 4
+	
+	li 	$t0 98
+	addiu	$v0 $v0 5
+	subu 	$a0 $t0 $v0
+	
+	bgeu	$a0 10 gt10 
+	b	lt10
+	
+Ende:	
+	li	$t4 22
+	la	$a1 Iban22
+	move 	$a0 $t6
+	
+	
+	move	$t7 $ra
+	
+	jal	Iban242Iban
+	
+	move	$ra $t7
+	
 	jr	$ra
+
+
+
+Blz2Iban:
+	lbu 	$t3 ($t1)
+	sb	$t3 ($a1)	
+	addiu	$t1 $t1 1
+	addiu	$a1 $a1 1
+	
+	subiu 	$t4 $t4 1
+	bnez 	$t4 Blz2Iban
+	jr 	$ra
+	
+Knr2Iban:
+	lbu 	$t3 ($t2)
+	sb	$t3 ($a1)	
+	addiu	$t2 $t2 1
+	addiu	$a1 $a1 1
+	
+	subiu 	$t4 $t4 1
+	bnez 	$t4 Knr2Iban
+	jr 	$ra
+	
+DE002Iban:
+	lbu 	$t3 ($a2)
+	sb	$t3 ($a1)	
+	addiu	$a2 $a2 1
+	addiu	$a1 $a1 1
+	
+	subiu 	$t4 $t4 1
+	bnez 	$t4 DE002Iban
+	jr 	$ra
+	
+Iban242Iban:
+	lbu 	$t3 ($a1)
+	sb	$t3 ($a0)	
+	addiu	$a0 $a0 1
+	addiu	$a1 $a1 1
+	
+	subiu 	$t4 $t4 1
+	bnez 	$t4 Iban242Iban
+	jr 	$ra
+	
+gt10:
+	la	$a1 Iban22
+	addiu	$a1 $a1 2
+	li	$t1 10
+	divu 	$t2 $a0 $t1
+	mfhi	$t3
+	addiu	$t2 $t2 48
+	addiu	$t3 $t3 48 
+	sb	$t2 ($a1)  
+	addiu	$a1 $a1 1
+	sb	$t3 ($a1)
+	
+	#la	$a1 Iban22
+	#addiu	$a1 $a1 2
+	#li	$a2 2
+	
+	#addu	$sp $sp -4
+	#sw 	$ra 0($sp)
+	
+	#jal 	int_to_buf
+	
+	#lw	$ra 0($sp)
+	#addu	$sp $sp 4
+	
+	b	Ende
+
+lt10:
+	la	$a1 Iban22
+	addiu	$a1 $a1 2
+	li 	$t1 48
+	sb	$t1 ($a1)
+	addiu	$a1 $a1 1
+	addiu	$a0 $a0 48
+	sb	$a0 ($a1)
+	b	Ende
--- Prog2/project1/src/knr2iban.s
+++ Prog2/project1/src/knr2iban.s
@@ -1,4 +1,8 @@
 	.data
+Iban24:
+	.space 23
+DE00:
+	.asciiz "DE00"
 	.globl knr2iban
 	.text
 # -- knr2iban
@@ -8,4 +12,103 @@
 # a2: KNR buffer (10 Zeichen)
 knr2iban:
 	# TODO
+	
+	move 	$t6 $a0
+	move 	$t1 $a1
+	move 	$t2 $a2
+	
+	la 	$a1 Iban24
+	la	$a2 DE00
+	move	$t7 $ra
+	li	$t4 6
+	jal	DE002Iban
+	li	$t4 8
+	jal	Blz2Iban
+	li	$t4 10
+	jal	Knr2Iban
+	move	$ra $t7
+	
+	addu	$sp $sp -4
+	sw 	$ra 0($sp)
+	
+	jal 	validate_checksum
+	
+	lw	$ra 0($sp)
+	addu	$sp $sp 4
+	
+	li 	$t0 98
+	subu 	$a0 $t0 $v0
+	
+	bgeu	$a0 10 gt10 
+	b	lt10
+	
+Ende:	
+	move	$a0 $t6 
+	la	$a1 Iban24
+	li	$a2 22
+	
+	addu	$sp $sp -4
+	sw 	$ra 0($sp)
+	
+	jal	memcpy
+	
+	lw	$ra 0($sp)
+	addu	$sp $sp 4
+	
 	jr	$ra
+
+
+
+Blz2Iban:
+	lbu 	$t3 ($t1)
+	sb	$t3 ($a1)	
+	addiu	$t1 $t1 1
+	addiu	$a1 $a1 1
+	
+	subiu 	$t4 $t4 1
+	bnez 	$t4 Blz2Iban
+	jr 	$ra
+	
+Knr2Iban:
+	lbu 	$t3 ($t2)
+	sb	$t3 ($a1)	
+	addiu	$t2 $t2 1
+	addiu	$a1 $a1 1
+	
+	subiu 	$t4 $t4 1
+	bnez 	$t4 Knr2Iban
+	jr 	$ra
+	
+DE002Iban:
+	lbu 	$t3 ($a2)
+	sb	$t3 ($a1)	
+	addiu	$a2 $a2 1
+	addiu	$a1 $a1 1
+	
+	subiu 	$t4 $t4 1
+	bnez 	$t4 DE002Iban
+	jr 	$ra
+	
+gt10:
+	la	$a1 Iban24
+	addiu	$a1 $a1 2
+	li	$a2 2
+	
+	addu	$sp $sp -4
+	sw 	$ra 0($sp)
+	
+	jal 	int_to_buf
+	
+	lw	$ra 0($sp)
+	addu	$sp $sp 4
+	
+	b	Ende
+
+lt10:
+	la	$a1 Iban24
+	addiu	$a1 $a1 2
+	li 	$t1 48
+	sb	$t1 ($a1)
+	addiu	$a1 $a1 1
+	sb	$a0 ($a1)
+	b	Ende
